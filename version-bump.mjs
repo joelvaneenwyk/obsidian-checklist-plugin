84779219126f33e8d7fa52ff88334a4759dacee4
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-11 15:04:47 -0700

    Upgrade packages and use corepack

--- version-bump.mjs
+++ version-bump.mjs
@@ -8,6 +8,12 @@ const {minAppVersion} = manifest
 manifest.version = targetVersion
 writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'))
 
+// read minAppVersion from manifest.json and bump version to target version
+let manifest_beta = JSON.parse(readFileSync('manifest-beta.json', 'utf8'))
+const {minAppVersionBeta} = manifest_beta
+manifest.version = `${targetVersion}-beta`
+writeFileSync('manifest-beta.json', JSON.stringify(manifest, null, '\t'))
+
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync('versions.json', 'utf8'))
 versions[targetVersion] = minAppVersion
